b016c5399508536be3b635cf3daccb975e5b628b
I.Safonov <safonovil17@yandex.ru>
2022-10-14 13:55:03 +0300

    Added solution second task to the third seminar

--- Seminar-3-HW.py
+++ Seminar-3-HW.py
@@ -7,12 +7,31 @@ os.system('cls')
 # *Пример:*
 # - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
-arr = np.random.randint(1, 10, 10)
-print(arr)
-sum = 0
-for i in range(len(arr)):
-    if i % 2 != 0:
-        sum += arr[i]
-        print(f'arr[{i}] = {arr[i]}')
-print('-----')
-print(f'Ответ: {sum}')
+# arr = np.random.randint(1, 10, 10)
+# print(arr)
+# sum = 0
+# for i in range(len(arr)):
+#     if i % 2 != 0:
+#         sum += arr[i]
+#         print(f'arr[{i}] = {arr[i]}')
+# print('-----')
+# print(f'Ответ: {sum}')
+
+
+# Задача 2. Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# *Пример:*
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+# arr = np.random.randint(1, 10, 5)
+# length = len(arr)
+# result = []
+# for i in range(length):
+#     while i < length/2 and length > length/2:
+#         length = length - 1
+#         mult = arr[i] * arr[length]
+#         result.append(mult)
+#         i += 1
+# print(arr)
+# print(result)
