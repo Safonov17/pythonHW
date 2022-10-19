5bea31f1bed7aee150a1c653e7f879a3482fe984
I.Safonov <safonovil17@yandex.ru>
2022-10-19 15:59:28 +0300

    Solution of the second task

--- Seminar-4-HW.py
+++ Seminar-4-HW.py
@@ -1,3 +1,4 @@
+import numpy as np
 import os
 os.system('cls')
 # задача 1. Задайте натуральное число N.
@@ -16,3 +17,20 @@ os.system('cls')
 #             i += 1
 #     return array
 # print(primes(num))
+
+# задача 2 . Задайте последовательность чисел.
+# Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+# array = np.random.randint(0, 10, 10)
+# print(f'Исходный массив: {array}')
+
+
+# def getUniqueNum(arr):
+#     newArray = []
+#     for i in arr:
+#         if i not in newArray:
+#             newArray.append(i)
+#     return newArray
+
+
+# print(getUniqueNum(array))
