4a27c99ba3712f3ae4166a9578f37764dba95072
I.Safonov <safonovil17@yandex.ru>
2022-10-19 14:56:02 +0300

    Solution of the first task

---
+++ Seminar-4-HW.py
@@ -0,0 +1,18 @@
+import os
+os.system('cls')
+# задача 1. Задайте натуральное число N.
+# Напишите программу, которая составит список простых множителей числа N.
+
+# num = int(input("Введите число: "))
+# def primes(x):
+#     i = 2
+#     array = []
+#     while i <= x:
+#         if x % i == 0:
+#             array.append(i)
+#             x //= i
+#             i = 2
+#         else:
+#             i += 1
+#     return array
+# print(primes(num))
