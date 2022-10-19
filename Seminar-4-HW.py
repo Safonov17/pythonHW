b40e570de198eae74f0cf3350e70ea4b77e123cf
I.Safonov <safonovil17@yandex.ru>
2022-10-19 16:57:18 +0300

    Solution of the fourth task

--- Seminar-4-HW.py
+++ Seminar-4-HW.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import os
 os.system('cls')
@@ -34,3 +35,49 @@ os.system('cls')
 
 
 # print(getUniqueNum(array))
+
+# задача 4.
+# Найдите корни квадратного уравнения, уравнение вводит через строку пользователь.
+# например, 6*x^2+5*x+6=0 . Само собой, уравнение может и не иметь решения.
+# Предусмотреть все варианты, сделать обработку исключений.
+
+
+# print('Введите коэффициенты для уравнения')
+# print('ax^2 + bx + c = 0:')
+# a = float(input('a = '))
+# b = float(input('b = '))
+# c = float(input('c = '))
+
+
+# if a != 0:
+#     if b != 0 and c != 0:
+#         D = b ** 2 - 4 * a * c
+#         print(f'D = {D}')
+#         if D < 0:
+#             print('Нет корней')
+#         elif D == 0:
+#             print('1 корень')
+#             x = -b / (2 * a)
+#             print(f'x = %.2f' % x)
+#         else:
+#             print('2 корня')
+#             x1 = (-b + math.sqrt(D)) / (2 * a)
+#             x2 = (-b - math.sqrt(D)) / (2 * a)
+#             print('x1 = %.2f \nx2 = %.2f' % (x1, x2))
+#     elif b == 0 and c == 0:
+#         print('Корень x = 0')
+#     elif b == 0:
+#         if -c / a < 0:
+#             print('Нет корней')
+#         else:
+#             print('2 корня')
+#             x1 = math.sqrt(-c / a)
+#             x2 = -x1
+#             print('x1 = %.2f \nx2 = %.2f' % (x1, x2))
+#     else:
+#         print('2 корня')
+#         x1 = 0
+#         x2 = -b / a
+#         print('x1 = %.2f \nx2 = %.2f' % (x1, x2))
+# else:
+#     print('a=0 => уравнение линейное')
